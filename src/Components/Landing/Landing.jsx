c474eec643804fb2095a3e73cd59501715a0f44f
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2023-02-08 18:42:13 +0530

    Add navbar and landing

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import bg from "../../Assets/images/landing_bg.svg";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div className="h-screen bg-gradient-to-b from-[#2A0637] to-black via-[#0D0221]">
+      <div className="w-full h-full flex justify-center items-center">
+        <img
+          src={bg}
+          alt=""
+          className="h-[564px] w-[1147px] absolute -z-0 mt-28"
+        />
+        <div className="font-pixeboy flex flex-col justify-center items-center">
+          <h3 className="text-white-off text-3xl">AUG12 - AUG13</h3>
+          <h1 className="text-[#8758FF] text-10xl text-shadow">CAD</h1>
+          <div className="text-white-off flex flex-row justify-around items-center w-full text-4xl text-shadow-landingSubtitle">
+            <h3>CONTROL</h3>
+            <h3>ALT</h3>
+            <h3>DELETE</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
