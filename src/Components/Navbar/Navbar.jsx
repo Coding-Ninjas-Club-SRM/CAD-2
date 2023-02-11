df2cdbcc45f8668513c2b75d893654dcd18e420b
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2023-02-11 21:57:36 +0530

    Make navbar and landing responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,9 +5,9 @@ import img from "../../Assets/images/navbar_img.svg";
 
 const Navbar = () => {
   return (
-    <div className="font-pixeboy text-white-off text-lg w-full fixed top-12 flex items-center justify-around hover:cursor-pointer z-10">
-      <img src={logo} alt="CN logo" />
-      <div className="h-12 w-[600px] rounded-lg drop-shadow-white bg-black bg-opacity-50 backdrop-blur-2xl backdrop-brightness-150">
+    <div className="font-pixeboy text-white-off text-base md:text-lg w-full fixed top-12 flex items-center justify-around hover:cursor-pointer z-10">
+      <img src={logo} alt="CN logo" className="lg:block hidden" />
+      <div className="h-12 w-[300px] sm:w-[400px] md:w-[600px] rounded-lg drop-shadow-white bg-black bg-opacity-50 backdrop-blur-2xl backdrop-brightness-150">
         <ul className="flex flex-row justify-evenly items-center h-full w-full">
           <li className="hover:scale-110 duration-200 hover:text-gray-200 hover:text-[#8758FF]">
             ABOUT
@@ -23,7 +23,7 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-      <img src={img} alt="" />
+      <img src={img} alt="" className="lg:block hidden" />
     </div>
   );
 };
