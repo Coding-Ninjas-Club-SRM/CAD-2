08982f823107582e91dcfc87639fe6ece3d94f5c
AYUSH <ayushrajbls@gmail.com>
2023-02-11 13:21:33 +0530

    Added footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return(
+    <div className="flex flex-col">
+
+
+    <div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <p>Creative dev. Itlay.</p>
+        <div>
+          <p>Sinalunga, Siena</p>
+          <p>53048</p>
+          <p>Piva 01553770528</p>
+          <p>Sine march 2022</p>
+        </div>
+      </div>
+      <div></div>
+    </div>
+
+    <div>
+      <p>Feel free to reach me anytime</p>
+      <p>Cookie Policy</p>
+    </div>
+    <div>
+      <p>The year is 2023</p>
+      <p>It is the future</p>
+    </div>
+    </div>
+  );
 };
 
 export default Footer;
